a84486e09a27eb2a14a0d2051821200329a74533
Leonardo Pellegrino <leonardo.pellegrino@avancoinfo.com.br>
2022-10-13 00:51:08 -0300

    feat: addm modules to main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,50 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './modules/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HomeCardComponent } from './modules/home-card/home-card.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatCardModule } from '@angular/material/card';
+import {MatDividerModule } from '@angular/material/divider';
+import {MatChipsModule } from '@angular/material/chips';
+import {MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule } from '@angular/material/icon';
+import {MatTabsModule } from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatProgressBarModule} from '@angular/material/progress-bar'
+import { MatNativeDateModule } from '@angular/material/core';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    HomeCardComponent
+    HomeCardComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatChipsModule,
+    MatToolbarModule,
+    MatIconModule
+
   ],
   providers: [],
   exports: [HomeCardComponent],
